57a73eba7f28c275a1dcdbc6255c4fa5806afd94
PavithraMuralidharan <77607394+PavithraMuralidharan@users.noreply.github.com>
2021-03-24 12:44:22 +0530

    Removed Duplications

--- src/main/java/sensorval/SensorValidator.java
+++ src/main/java/sensorval/SensorValidator.java
@@ -1,31 +1,40 @@
-package sensorval;
+package main.java.sensorval;
 
 import java.util.List;
 
 public class SensorValidator 
 {
-    public static boolean _give_me_a_good_name(double value, double nextValue, double maxDelta) {
-        if(nextValue - value > maxDelta) {
-            return false;
-        }
-        return true;
-    }
-    public static boolean validateSOCreadings(List<Double> values) {
-        int lastButOneIndex = values.size() - 1;
-        for(int i = 0; i < lastButOneIndex; i++) {
-            if(!_give_me_a_good_name(values.get(i), values.get(i + 1), 0.05)) {
-            return false;
-            }
-        }
-        return true;
-    }
-    public static boolean validateCurrentreadings(List<Double> values) {
-        int lastButOneIndex = values.size() - 1;
-        for(int i = 0; i < lastButOneIndex; i++) {
-            if(!_give_me_a_good_name(values.get(i), values.get(i + 1), 0.1)) {
-            return false;
-            }
-        }
-        return true;
-    }
+	static float maxSOCDelta = 0.1f;
+	static float maxCurrentDelta = 0.05f;
+
+	SensorValidator() {
+	}
+
+	public static boolean isSuddenJump(double value, double nextValue, double maxDelta) {
+		return (nextValue - value > maxDelta);
+	}
+
+	public static boolean validateSOCreadings(List<Double> values) 
+	{
+		return isNull(values) ? validateReadings(values, maxSOCDelta) : false;
+	}
+
+	public static boolean validateCurrentreadings(List<Double> values) {
+		return isNull(values) ? validateReadings(values, maxCurrentDelta) : false;
+	}
+
+	public static boolean validateReadings(List<Double> values, float maxDelta) {
+		int lastButOneIndex = values.size() - 1;
+		for (int i = 0; i < lastButOneIndex; i++) {
+			if (!isSuddenJump(values.get(i), values.get(i + 1), maxDelta)) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	public static boolean isNull(List<Double> values)
+	{
+		return (values.isEmpty() && values != null);
+	}
 }
